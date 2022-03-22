d6dbf3c654ff3d88e6cbe6c9f8d3568a959c39fe
Toriqul-Leon <yeitoriqul@gmail.com>
2022-03-22 16:50:01 +0600

    Updates

---
+++ precache-manifest.1e7245676fbbc21fc1fe98c2c68a301d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "164fd9c430d85c1dd434",
+    "url": "/amazon_about_to_be_mocked_react/static/css/main.055d3c65.chunk.css"
+  },
+  {
+    "revision": "164fd9c430d85c1dd434",
+    "url": "/amazon_about_to_be_mocked_react/static/js/main.164fd9c4.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/amazon_about_to_be_mocked_react/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "df8825f2aa7092963e45",
+    "url": "/amazon_about_to_be_mocked_react/static/js/2.df8825f2.chunk.js"
+  },
+  {
+    "revision": "df3dbd7f2d3373b59a25",
+    "url": "/amazon_about_to_be_mocked_react/static/js/runtime~main.df3dbd7f.js"
+  },
+  {
+    "revision": "af182446517842baab598545221166e6",
+    "url": "/amazon_about_to_be_mocked_react/static/media/Logo.af182446.svg"
+  },
+  {
+    "revision": "1c3ccf2dce512c782bfd67cb92f888fc",
+    "url": "/amazon_about_to_be_mocked_react/index.html"
+  }
+];
