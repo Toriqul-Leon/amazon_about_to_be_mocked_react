9246e73f4bda2e3601f31014498420f1fece36a7
Toriqul-Leon <yeitoriqul@gmail.com>
2022-04-10 20:54:58 +0600

    Form done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,31 +4,50 @@ import "./Login.css";
 
 const Login = () => {
   return (
-    <div className="container">
-      <div className="form-container">
-        <div>
-          {" "}
-          <h1 className="form-title">Login</h1>
-          <form action="">
-            <div className="input-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="" />
+    <>
+      <div className="container">
+        <div className="form-container">
+          <div>
+            {" "}
+            <h1 className="form-title">Login</h1>
+            <form action="">
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input required type="email" name="email" id="" />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input required type="password" name="password" id="" />
+              </div>
+              <input
+                required
+                className="form-submit"
+                type="submit"
+                value="Login"
+              />
+            </form>
+            <p>
+              New to Amazon?{" "}
+              <Link className="form-link" to="/signup">
+                Create an Account
+              </Link>
+            </p>
+            <div className="line-view">
+              <div className="line"></div>
+              <span>or</span>
+              <div className="line"></div>
             </div>
-            <div className="input-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="" />
+            <div className="google-logo">
+              <img
+                src="https://www.clipartmax.com/png/middle/219-2197783_training-documents-google-logo-icon-png.png"
+                alt=""
+              />
+              <span>Continue with Google </span>
             </div>
-            <input className="form-submit" type="submit" value="Login" />
-          </form>
-          <p>
-            New to Amazon?{" "}
-            <Link className="form-link" to="/signup">
-              Create an Account
-            </Link>
-          </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
-    </div>
+    <>
+      <div className="container">
+        <div className="form-container">
+          <div>
+            {" "}
+            <h1 className="form-title">Sign Up</h1>
+            <form action="">
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input required type="email" name="email" id="" />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input required type="password" name="password" id="" />
+              </div>
+              <div className="input-group">
+                <label htmlFor="confirm-password">Confirm Password</label>
+                <input required type="password" name="confirm-password" id="" />
+              </div>
+              <input className="form-submit" type="submit" value="Sign Up" />
+            </form>
+            <p>
+              Already Have an Account?{" "}
+              <Link className="form-link" to="/login">
+                Login
+              </Link>
+            </p>
+            <div className="line-view">
+              <div className="line"></div>
+              <span>or</span>
+              <div className="line"></div>
+            </div>
+            <div className="google-logo">
+              <img
+                src="https://www.clipartmax.com/png/middle/219-2197783_training-documents-google-logo-icon-png.png"
+                alt=""
+              />
+              <span>Continue with Google </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
